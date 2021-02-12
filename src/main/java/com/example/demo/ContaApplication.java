8c20150b0e7365840c5f57a08ba87e5fd014c0af
Cabeça <Cabea>
2021-02-11 21:33:14 -0300

    primeiro commit

--- src/main/java/com/example/demo/ContaApplication.java
+++ src/main/java/com/example/demo/ContaApplication.java
@@ -1,11 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.entity.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ContaApplication {
 
-	public static void main(String[] args) {SpringApplication.run(ContaApplication.class, args); }
+	public static void main(String[] args) {
+		final ConfigurableApplicationContext context = SpringApplication.run(ContaApplication.class, args);
+		JwtService service = context.getBean(JwtService.class);
+		final Usuario usuario = Usuario.builder()
+				.id("1")
+				.login("Cabeça")
+				.build();
+
+		;
+		System.out.println(service.gerarToken(usuario));
+	}
 
 }
--- src/main/java/com/example/demo/web/rest/resource/ContaResource.java
+++ src/main/java/com/example/demo/web/rest/resource/ContaResource.java
@@ -1,29 +1,33 @@
 package com.example.demo.web.rest.resource;
 
 
-import com.example.demo.web.rest.exception.OperacaoNaoAutorizadaException;
 import com.example.demo.service.ContaService;
 import com.example.demo.web.rest.dto.ContaDTO;
 import com.example.demo.web.rest.dto.request.DepositoDTO;
 import com.example.demo.web.rest.dto.request.SaqueDTO;
 import com.example.demo.web.rest.dto.request.TransferenciaDTO;
 import com.example.demo.web.rest.dto.response.ContaRespostaDTO;
+import com.example.demo.web.rest.exception.OperacaoNaoAutorizadaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.naming.OperationNotSupportedException;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/conta")
 public class ContaResource {
 
+    final private ContaService contaService;
+
     @Autowired
-    private ContaService contaService;
+    public ContaResource(ContaService contaService) {
+        this.contaService = contaService;
+    }
 
     @PostMapping
-    public ResponseEntity<Object> cadastraConta(@RequestBody ContaDTO contaDTO) throws IOException {
+    public ResponseEntity<Object> cadastraConta(@RequestBody ContaDTO contaDTO) {
         return ResponseEntity.ok(new ContaRespostaDTO(contaService.salvaConta(contaDTO).getNumeroConta(), "Conta cadastrada com sucesso!"));
 
     }
@@ -35,8 +39,8 @@ public class ContaResource {
     }
 
     @PostMapping("/saque")
-    public ResponseEntity<Object> sacarDaConta(@RequestBody SaqueDTO saqueDTO) throws OperationNotSupportedException {
-        contaService.realizaSaque(saqueDTO);
+    public ResponseEntity<Object> sacarDaConta(@RequestBody SaqueDTO saqueDTO) {
+        contaService.realizarSaque(saqueDTO);
         return ResponseEntity.ok(new ContaRespostaDTO("Saque realizado com sucesso!"));
     }
 
